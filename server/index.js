f6f133ef17a2c8f283cb538033adc1da323be4c8
yash01491625 <yyashmalviya051@gmail.com>
2025-05-12 19:48:34 +0530

    init

--- server/index.js
+++ server/index.js
@@ -36,7 +36,7 @@ const server = app.listen(process.env.PORT, () =>
 );
 const io = socket(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://chatbox-client.vercel.app/",
     credentials: true,
   },
 });
